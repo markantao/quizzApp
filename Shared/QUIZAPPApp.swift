320f712f1097f8f353de503a963b5aa2bc01abab
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-03-15 15:59:50 -0400

    Initial Commit

---
+++ Shared/QUIZAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  QUIZAPPApp.swift
+//  Shared
+//
+//  Created by Mark Antao on 2021-03-15.
+//
+
+import SwiftUI
+
+@main
+struct QUIZAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
